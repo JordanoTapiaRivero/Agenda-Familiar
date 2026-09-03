f51a6022e46cb6b327204698ab16a8b393a90f6d
Jordano Tapia <hth8gv9925@privaterelay.appleid.com>
2026-09-03 13:28:39 -0400

    errores

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { supabase } from '../lib/supabaseClient'
-import './Login.css'
+import './login.css'
 
 function Login({ onLogin }) {
   const [modo, setModo] = useState('login')
